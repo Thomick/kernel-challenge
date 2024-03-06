d9efdc9eec6a0e893f583ccfb3d2058e781e21de
Thomas MICHEL <thomas.michel.thmi@gmail.com>
2024-03-06 23:41:38 +0100

    Fix fisher vector

--- src/fisher_vector.py
+++ src/fisher_vector.py
@@ -5,142 +5,46 @@ import numpy as np
 # TODO : Check if the code is correct
 
 
-class FisherVector:
+def compute_fisher_vector(data, gmm):
     """
-    Class to compute Fisher Vectors from local features
+    Compute the Fisher Vector of a dataset using the specified Gaussian Mixture Model
 
-    Assumes that the local features are extracted from a GMM with diagonal covariance matrices
-
-    Attributes:
-    - n_component: number of Gaussians in the GMM
-    - dimension: dimension of the local features
-    - mixture_weight: proportion of the Gaussian in the GMM (n_component)
-    - mus: the means of the GMM (n_component x dimension)
-    - sigmas: the variances of the GMM (n_component x dimension)
-
+    Arguments:
+    - data: a 2D numpy array representing the dataset
+    - gmm: a GaussianMixture object
     """
+    mixture_weight = gmm.weights_
+    means = gmm.means_
+    covariances = gmm.covariances_
+    n_samples, n_features = data.shape
+    n_components = len(mixture_weight)
+    # Compute the posterior probabilities
+    posterior = gmm.predict_proba(data)
+    # Compute the sufficient statistics
+    S = posterior.sum(axis=0, keepdims=True).T
+    x = posterior.T @ data/ n_samples
+    x2 = posterior.T @ (np.power(data, 2)) / n_samples
+    # Compute GMM gradients
+    d_pi = (S) - mixture_weight
+    d_mu = x - S*means
+    d_sigma = -x2 - S*np.power(means, 2) + S*covariances + 2*x*means
+    # Normalize the gradients
+    sqrt_pi = np.sqrt(mixture_weight)
+    d_pi /= sqrt_pi
+    d_mu /= sqrt_pi[:, np.newaxis] * np.sqrt(covariances)
+    d_sigma /=  sqrt_pi[:, np.newaxis] * covariances * np.sqrt(2)
 
-    def __init__(self, n_component, dimension, mixture_weight, mus, sigmas):
-        self.n_component = n_component
-        self.dimension = dimension
-        self.mixture_weight = mixture_weight
-        self.mus = mus
-        self.sigmas = sigmas
+    # Concatenate the gradients
+    fv = np.concatenate((d_pi.reshape(1,-1), d_mu.reshape(1,-1), d_sigma.reshape(1,-1)), axis=1)
 
-    def compute_fisher_vector(self, features):
-        """
-        Compute the Fisher Vector from a set of local features
-
-        Arguments:
-        - features: a 2D numpy array containing the local features (n_samples x dimension)
-
-        Returns:
-        - fv: a 1D numpy array containing the Fisher Vector
-        """
-        # Compute the posterior probabilities
-        # (n_samples x n_component)
-        post_prob = self.compute_posterior_probabilities(features)
-
-        # Compute the Fisher Vector
-        fv = self.compute_fisher_vector_from_posterior_probabilities(
-            features, post_prob
-        )
-
-        return fv
-
-    def compute_posterior_probabilities(self, features):
-        """
-        Compute the posterior probabilities of the local features
-
-        Arguments:
-        - features: a 2D numpy array containing the local features (n_samples x dimension)
-
-        Returns:
-        - post_prob: a 2D numpy array containing the posterior probabilities (n_samples x n_component)
-        """
-        # Compute the likelihood
-        # (n_samples x n_component)
-        likelihood = self.compute_likelihood(features)
-
-        # Compute the posterior probabilities
-        # (n_samples x n_component)
-        post_prob = likelihood * self.mixture_weight
-        post_prob /= post_prob.sum(axis=1, keepdims=True)
-
-        return post_prob
-
-    def compute_likelihood(self, features):
-        """
-        Compute the likelihood of the local features
-
-        Arguments:
-        - features: a 2D numpy array containing the local features (n_samples x dimension)
-
-        Returns:
-        - likelihood: a 2D numpy array containing the likelihood (n_samples x n_component)
-        """
-        # Compute the likelihood
-        # (n_samples x n_component)
-        likelihood = np.exp(
-            -0.5
-            * ((features[:, None, :] - self.mus[None, :, :]) ** 2)
-            / self.sigmas[None, :, :]
-        )
-        likelihood /= np.sqrt(self.sigmas * 2 * np.pi)
-
-        return likelihood
-
-    def compute_fisher_vector_from_posterior_probabilities(self, features, post_prob):
-        """
-        Compute the Fisher Vector from the posterior probabilities
-
-        Arguments:
-        - features: a 2D numpy array containing the local features (n_samples x dimension)
-        - post_prob: a 2D numpy array containing the posterior probabilities (n_samples x n_component)
-
-        Returns:
-        - fv: a 1D numpy array containing the Fisher Vector
-        """
-        # Compute the Fisher Vector
-        # (n_component x dimension)
-        grad = self.compute_gradient(features, post_prob)
-        grad = grad.reshape(-1)
-
-        # L2 normalize the Fisher Vector
-        fv = grad / np.sqrt(grad.dot(grad))
-
-        return fv
-
-    def compute_gradient(self, features, post_prob):
-        """
-        Compute the gradient of the Fisher Vector
-
-        Arguments:
-        - features: a 2D numpy array containing the local features (n_samples x dimension)
-        - post_prob: a 2D numpy array containing the posterior probabilities (n_samples x n_component)
-
-        Returns:
-        - grad: a 2D numpy array containing the gradient of the Fisher Vector (n_component x dimension)
-        """
-        # Compute the gradient
-        # (n_component x dimension)
-        grad = np.zeros((self.n_component, self.dimension))
-        for i in range(self.n_component):
-            diff = features - self.mus[i]
-            grad[i] = (diff * post_prob[:, i, None]).sum(axis=0)
-            grad[i] /= np.sqrt(self.mixture_weight[i])
-
-        return grad
+    return fv 
+    # TODO: Check the improved version of the Fisher Vector (power normalization, L2 normalization)
 
 
 if __name__ == "__main__":
-    n_component = 2
-    dimension = 2
-    mixture_weight = np.array([0.5, 0.5])
-    mus = np.array([[0, 0], [1, 1]])
-    sigmas = np.array([[1, 1], [1, 1]])
-
-    fv = FisherVector(n_component, dimension, mixture_weight, mus, sigmas)
-
-    features = np.array([[1, 0]])
-    print(fv.compute_fisher_vector(features))
+    from sklearn.mixture import GaussianMixture
+    data = np.array([[1, 2, 3], [4, 5, 7], [1, 2, 4], [5, 6, 77], [2, 3, 4]])
+    gmm = GaussianMixture(n_components=3, covariance_type='diag')
+    gmm.fit(data)
+    fv = compute_fisher_vector(data[0][None,:], gmm)
+    print(fv)
